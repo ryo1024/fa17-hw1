b15fe04fe20be4248b82a92bd226809d68ce1c55
Ryo Yamamoto <ryo10244201@berkeley.edu>
2017-09-17 22:09:49 -0700

    done

--- hw1.rb
+++ hw1.rb
@@ -1,20 +1,25 @@
+
 def squared_sum(a, b)
-  # Q1 CODE HERE
+
+ return (a.to_i + b.to_i) * (a.to_i + b.to_i)
 
 end
+
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+
+ary_b=a.sort
+
+a.sort!
+
+ary_b.map {|x| x + 1} 
 
 end
+
+
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
 
-end
-
-def blockin_time(a)
-  # DO NOT EDIT THIS CODE BELOW
-  require './foobar'
-  Foobar.baz a
+output_name =  '"'+ first_name +" "+ last_name + '"'
+puts output_name
 end
